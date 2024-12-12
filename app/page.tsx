b60476ef3c697559770d8206b5a6042cc7f87580
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2024-12-12 14:01:46 +0530

    updated with testimonials section

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,18 @@ import Features from "./components/Features";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      {/* https://www.youtube.com/watch?v=ukiGFmZ32YA&ab_channel=JavaScriptMastery @ 2:16:56 */}
+      {/* https://www.youtube.com/watch?v=ukiGFmZ32YA&ab_channel=JavaScriptMastery @ 2:28:15 */}
       <Header />
       <Hero />
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
     </main>
   );
 }
