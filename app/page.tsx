c4b32f5f9c546f60c9b28f3f2ac364fbb6b43e3d
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2024-12-12 15:52:45 +0530

    updated with download section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Download from "./components/Download";
 import Faq from "./components/Faq";
 import Features from "./components/Features";
 import Header from "./components/Header";
@@ -8,13 +9,14 @@ import Testimonials from "./components/Testimonials";
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      {/* https://www.youtube.com/watch?v=ukiGFmZ32YA&ab_channel=JavaScriptMastery @ 2:28:15 */}
+      {/* https://www.youtube.com/watch?v=ukiGFmZ32YA&ab_channel=JavaScriptMastery @ 2:40:52 */}
       <Header />
       <Hero />
       <Features />
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
     </main>
   );
 }
